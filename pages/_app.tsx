f4d97f585d8b8e3c86ebbd962cf925ef70c1bba9
QuentinJs1 <contact@quentinjs.com>
2022-11-14 17:00:50 +0100

    :mag: tag seo _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,36 @@ import type { AppProps } from 'next/app'
 import Banner from "@components/Banner";
 import Navbar from "@components/Navbar";
 import Bottombar from '@components/BottomBar';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextSeo
+        title="Quentin.js"
+        titleTemplate="Quentin.js"
+        defaultTitle="Quentin.js"
+        description="Creative developer français, je suis capable de créer des expériences web 3D interactives en tant que freelance."
+        canonical="https://quentinjs.com/"
+        openGraph={{
+          url: "https://quentinjs.com/",
+          title: "Quentin.js",
+          description: "Creative developer français, je suis capable de créer des expériences web 3D interactives en tant que freelance.",
+          images: [
+            {
+              url: "/og-image.jpg",
+              width: 800,
+              height: 420,
+              alt: "Quentin.js",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@quentin__js",
+          site: "@quentin__js",
+          cardType: "summary_large_image",
+        }}
+      />
       <Banner />
       <Navbar />
       <Component {...pageProps} />
